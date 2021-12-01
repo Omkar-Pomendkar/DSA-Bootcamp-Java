8572a76ce0a2663e6f75eb391a4b300792aa80e3
omkarsp121 <omkarpomendkar100@gmail.com>
2021-12-01 19:09:02 +0530

    leap year

--- Programs & Topics/programs/practiceset2/ps2_Float_Result.java
+++ Programs & Topics/programs/practiceset2/ps2_Float_Result.java
@@ -2,7 +2,7 @@ package programs.practiceset2;
 
 public class ps2_Float_Result {
     public static void main(String[] args) {
-        System.out.println("What will be the Result of the Expression");
+        System.out.println("What will be the Result of the Expresssion");
         float a = (7/4f)*(9/2f);
         System.out.println(a);
     }
