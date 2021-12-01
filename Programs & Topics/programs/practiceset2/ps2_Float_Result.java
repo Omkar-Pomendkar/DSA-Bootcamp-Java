ef4d9e90897163a32633fea21eb549526f5acd2a
omkarsp121 <omkarpomendkar100@gmail.com>
2021-12-01 17:21:04 +0530

    Solving Practice Set 2

---
+++ Programs & Topics/programs/practiceset2/ps2_Float_Result.java
@@ -0,0 +1,9 @@
+package programs.practiceset2;
+
+public class ps2_Float_Result {
+    public static void main(String[] args) {
+        System.out.println("What will be the Result of the Expression");
+        float a = (7/4f)*(9/2f);
+        System.out.println(a);
+    }
+}
