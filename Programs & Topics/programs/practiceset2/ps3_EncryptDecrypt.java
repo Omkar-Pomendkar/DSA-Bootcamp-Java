b1fc83bfc5fc806fdae68997b94e782fd77a1044
omkarsp121 <omkarpomendkar100@gmail.com>
2021-12-01 17:29:53 +0530

    Solving Practice Set 2

---
+++ Programs & Topics/programs/practiceset2/ps3_EncryptDecrypt.java
@@ -0,0 +1,16 @@
+package programs.practiceset2;
+
+import java.util.Scanner;
+
+public class ps3_EncryptDecrypt {
+    public static void main(String[] args) {
+        System.out.println("Encrypt a & Decrypt a");
+        char alphabet ='a';
+        int total = (char)alphabet + 8;
+        System.out.println(total);
+
+//        char alphabet ='a';
+        int total1 = (char)alphabet - 8;
+        System.out.println(total1);
+    }
+}
